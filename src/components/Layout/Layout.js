0a0a51ab22cd83f195cdd50f908189d98d47f649
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-09-05 12:34:20 +0000

    added navigation bar with logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,15 @@ import React from "react";
 
 // high order component, used to wrap adjecent jsx elements
 import Aux from "../../hoc/Aux";
+//
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 // import CSS classes
 import classes from "./Layout.css";
 
 const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className = {classes.Content}>
       {props.children}
     </main>
