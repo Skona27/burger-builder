3c6e3b7a11461cb9fb99f9d4047b02f3ab36132e
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-09-05 13:11:43 +0000

    added nav items to navbar

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./NavigationItems.css";
+
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const navigationItems = props => (
+    <ul className={classes.NavItems}>
+      <NavigationItem link="/" active>Link 1</NavigationItem>
+      <NavigationItem link="/" >Link 2</NavigationItem>
+    </ul>
+)
+
+export default navigationItems;
