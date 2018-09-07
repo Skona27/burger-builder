f00702a7999a83100c60399284ddb109f07004ef
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-09-07 09:49:45 +0000

    added sidemenu and toggle button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,46 @@
 // import react library
-import React from "react";
+import React, {Component} from "react";
 
 // high order component, used to wrap adjecent jsx elements
 import Aux from "../../hoc/Aux";
 //
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 // import CSS classes
 import classes from "./Layout.css";
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <main className = {classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      showSideDrawer: false
+    };
+  } 
+  
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+  
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return {showSideDrawer: !prevState.showSideDrawer};
+    });
+  }
+  
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer isOpened={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className = {classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
 
 
-export default layout;
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,17 @@ import classes from "./Toolbar.css";
 
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo />
-    <NavigationItems />
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div style={{height: "80%"}}>
+      <Logo />
+    </div>
+    <nav className={classes.Desktop}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
